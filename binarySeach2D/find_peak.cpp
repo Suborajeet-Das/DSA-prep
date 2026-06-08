c7aa6ba1cffad939cdd044a9353d3510f3dbfe59
Suborajeet-Das <subrodazz@gmail.com>
2026-06-08 23:24:49 +0530

    set the question

--- binarySeach2D/find_peak.cpp
+++ binarySeach2D/find_peak.cpp
@@ -1,6 +1,11 @@
+//find the peak element in a 2D matrix
+
 #include<iostream>
+#include<vector>
 using namespace std; 
 
 int main(){
+
+  vector<vector<int>> matrix = {{1,4},{3,2}};
   return 0;
 }
