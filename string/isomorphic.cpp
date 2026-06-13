afa8116f74d592c71548d3d9242572925daa6676
Suborajeet-Das <subrodazz@gmail.com>
2026-06-13 23:57:56 +0530

    isomorphic

---
+++ string/isomorphic.cpp
@@ -0,0 +1,25 @@
+
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> st;
+        unordered_map<char, char> ts;
+
+        for(int i = 0; i < s.size(); i++) {
+            char a = s[i];
+            char b = t[i];
+
+            if(st.count(a) && st[a] != b)
+                return false;
+
+            if(ts.count(b) && ts[b] != a)
+                return false;
+
+            st[a] = b;
+            ts[b] = a;
+        }
+
+        return true;
+    }
+};
