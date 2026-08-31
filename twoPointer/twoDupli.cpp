96129c48ceb8f8772c244bf9d0886fc03a44f909
Suborajeet-Das <subrodazz@gmail.com>
2026-08-31 18:25:22 +0530

    two duplicates allowed

---
+++ twoPointer/twoDupli.cpp
@@ -0,0 +1,28 @@
+//80
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() < 3) return nums.size();
+        int j = 2;
+        for(int i=2; i<nums.size(); i++){
+            if(nums[i] != nums[j-2]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+};
+
+int main(){
+  vector<int> nums = {0,0,1,1,1,1,2,3,3};
+  Solution a;
+  int k = a.removeDuplicates(nums);
+
+  cout << k;
+}
